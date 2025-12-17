6a706a7835a3cb34e701b88666839727729c5fa3
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-17 09:39:04 +0100

    93812172025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FinancesComponent } from './finances/finances.component';
+import { GameComponent } from './game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Login', component:LoginComponent},
+    {path:'Register', component:RegisterComponent},
+    {path:'Finances', component:FinancesComponent},
+    {path:'Game', component:GameComponent}
+];
