b7a89d33827ea8954ff6630d6e15baff484d06a2
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-17 10:31:05 +0100

    Angular version migration

--- src/app/finances/finances.component.ts
+++ src/app/finances/finances.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-finances',
-  standalone: true,
-  imports: [],
-  templateUrl: './finances.component.html',
-  styleUrl: './finances.component.css'
+    selector: 'app-finances',
+    imports: [],
+    templateUrl: './finances.component.html',
+    styleUrl: './finances.component.css'
 })
 export class FinancesComponent {
 
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+    selector: 'app-game',
+    imports: [],
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.css'
 })
 export class GameComponent {
 
