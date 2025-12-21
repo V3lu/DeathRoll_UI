544047e2eeeb5fa4f98fcdb85e9fbceccdccd05b
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-21 09:58:02 +0100

    95712212025

--- Services/apiconnection.service.ts
+++ Services/apiconnection.service.ts
@@ -8,10 +8,31 @@ import { JwtTokenContainerService } from './jwt-token-container.service';
 export class APIConnectionService {
 
   loginEndpoint = "http://localhost:8080/Auth/Login";
+  registerEndpoint = "http://localhost:8080/Auth/Register";
+  CheckEmailViabilityEndpoint = "http://localhost:8080/Auth/CheckEmailViability";
+  CheckUsernameViabilityEndpoint = "http://localhost:8080/Auth/CheckUsernameViability";
 
   constructor(private http : HttpClient, private tokenContainer : JwtTokenContainerService){}
 
   login(username : string, hashedPassword : string){
     return this.http.post<any>(this.loginEndpoint, {username, hashedPassword}, {observe: 'response'});
   }
+
+  register(username : string, hashedPassword : string, email : string){
+    return this.http.post<any>(this.registerEndpoint, {username, hashedPassword, email}, {observe: 'response'});
+  }
+
+  CheckUsernameViability(username : string){
+    return this.http.post<any>(this.CheckUsernameViabilityEndpoint, {username}, {observe: 'response'});
+  }
+
+  CheckEmailViability(email : string){
+    return this.http.post<any>(this.CheckEmailViabilityEndpoint, {email}, {observe: 'response'});
+  }
+
+
+
+
+
+
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,140 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal, Signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APIConnectionService } from '../../../Services/apiconnection.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
     selector: 'app-register',
-    imports: [],
+    imports: [RouterModule, CommonModule],
     templateUrl: './register.component.html',
     styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+    isUsernameProper : boolean = true;
+    isEmailProper : boolean = true;
+    isPasswordProper : boolean = true;
+
+    passwordIcon : boolean = false;
+    emailIcon : boolean = false;
+    nameIcon : boolean = false;
+
+    nameSignal = signal('');
+    emailSignal = signal('');
+    passwordSignal = signal('');
+
+    registerForm : FormGroup;
+
+    constructor(private apiconn : APIConnectionService, private fb : FormBuilder){
+        this.registerForm = this.fb.group({
+            Name : [''],
+            Email : [''],
+            Password : ['']
+        })
+    }
+
+    Register(event : Event){
+        event.preventDefault();
+        this.apiconn.register(this.registerForm.value.Name, this.registerForm.value.Email, this.registerForm.value.Password)
+    }
+
+    assignUsername(event : Event){
+        const value = (event.target as HTMLInputElement).value;
+        this.nameSignal.set(value);
+
+        if (this.nameSignal().length > 0){
+            this.apiconn.CheckUsernameViability(this.nameSignal())
+            .pipe(
+                map((response) => {
+                    console.log(response);
+                    return {response}
+                }),
+                catchError((err) => {
+                    if (err.status == 409){
+                        this.isUsernameProper = false;
+                        document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+                        this.nameIcon = false;
+                    }
+                    return throwError(() => new Error(err));
+                })
+            ).subscribe({
+                next: ((response) => {
+                    this.isUsernameProper = true;
+                    this.tryUnlockSubmitButton();
+                    this.nameIcon = true;
+                })
+            })
+        }
+        else{
+            this.isUsernameProper = true;
+            document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+            this.nameIcon = false;
+        }
+    }
+
+    assignEmail(event : Event){
+        const value = (event.target as HTMLInputElement).value;
+        this.emailSignal.set(value);
+
+        if (this.emailSignal().length > 0){
+            this.apiconn.CheckEmailViability(this.emailSignal())
+            .pipe(
+                map((response) => {
+                    console.log(response);
+                    return {response}
+                }),
+                catchError((err) => {
+                    if (err.status == 409){
+                        this.isEmailProper = false;
+                        document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+                        this.emailIcon = false;
+                    }
+                    return throwError(() => new Error(err));
+                })
+            ).subscribe({
+                next: ((response) => {
+                    this.isEmailProper = true;
+                    this.tryUnlockSubmitButton();
+                    this.emailIcon = true;
+                })
+            })
+        }
+        else{
+            this.isEmailProper = true;
+            document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+            this.emailIcon = false;
+        }
+    }
+
+    assignPassword(event : Event){
+        const value = (event.target as HTMLInputElement).value;
+        this.passwordSignal.set(value);
+
+        const valueSignal = this.passwordSignal();
+        const hasNumber = valueSignal.match("[0-9]+");
+        const hasSpecialChar = valueSignal.match("[^A-Za-z0-9]");
+
+        if(valueSignal.length < 8 || hasNumber?.length == null || hasSpecialChar?.length == null){
+            this.passwordIcon = false;
+            this.isPasswordProper = false;
+            document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+        }
+
+        if(valueSignal.length >= 8 && hasNumber?.length != null && hasSpecialChar?.length != null){
+            this.passwordIcon = true;
+            this.isPasswordProper = true;
+            this.tryUnlockSubmitButton();
+        }
+    }
+
+    tryUnlockSubmitButton(){
+        if (this.passwordSignal() != '' && this.emailSignal() != '' && this.nameSignal() != ''){
+            document.getElementById("btnsub")?.removeAttribute('disabled');
+        }
+        else{
+            document.getElementById("btnsub")?.setAttribute('disabled', 'disabled');
+        }
+    }
 
 }
